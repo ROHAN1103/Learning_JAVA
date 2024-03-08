28299fa886391bc01f6a0e078a353d21845a2998
Rohan <120027563+ROHAN1103@users.noreply.github.com>
2024-03-08 21:46:30 +0530

    Add files via upload

---
+++ create_obj.java
@@ -0,0 +1,15 @@
+public class create_obj {
+    void dispaly() {
+        System.out.println("SKADOOSH");
+    }
+
+    public static void main(String args[]) {
+        create_obj x = new create_obj();
+        {
+            System.out.println("I am Speed");
+        }
+        x.dispaly();
+
+    }
+
+}
