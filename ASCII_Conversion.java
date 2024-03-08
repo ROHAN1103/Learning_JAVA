88c4c1d151fb0f921de23596d09efbcae4f4fd0b
Rohan <120027563+ROHAN1103@users.noreply.github.com>
2024-03-08 21:47:11 +0530

    Add files via upload

---
+++ ASCII_Conversion.java
@@ -0,0 +1,23 @@
+public class ASCII_Conversion {
+    public static void main(String args[])
+    {
+        char a1,a2,a3;
+        a1='a';
+        a2='z';
+        a3='r';
+        int b1=a1;
+        int b2=a2;
+        int b3='@';
+        int b4='s';
+        int b5=(int)a3;
+
+        System.out.println("The Ascii value of "+a1+" is "+b1);
+        System.out.println("The Ascii value of "+a2+" is "+b2);
+        System.out.println("The Ascii value of @ is "+b3);
+        System.out.println("The Ascii value of s is "+b4);
+        System.out.println("The Ascii value of "+a3+" is "+b5);
+
+        
+    }
+    
+}
---
+++ Ascii_For_All.java
@@ -0,0 +1,11 @@
+public class Ascii_For_All {
+    public static void main(String args[])
+    {
+        for(int i=34;i<124;i++)
+        {
+            System.out.println("The ascii value of "+(char)i+" is "+i);
+        }
+
+    }
+    
+}
